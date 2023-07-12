1be7689bf086d29a084d98df4da3cd8d3f693725
GuiFigueiredo <guilhermeandra@hotmail.com>
2023-07-12 01:10:39 -0300

    Primeiro programa criado na pasta Codigos_python

---
+++ Codigos_Python/Media_Aluno.py
@@ -0,0 +1,11 @@
+## programa para calcular média de um aluno
+
+nota = float(input('Insira a nota do aluno: '))
+nota2 = float(input('Insira a nota do aluno: '))
+nota3 = float(input('Insira a nota do aluno: '))
+nota4 = float(input('Insira a nota do aluno: '))
+
+media = (nota + nota2 + nota3 +nota4) /4
+
+print(f' a media do aluno é {media}')
+
